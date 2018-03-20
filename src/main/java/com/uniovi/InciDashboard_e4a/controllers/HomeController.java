d1623087d2f934c926beb604930dc24b037e5114
Antonio Paya <antonioalfa22@gmail.com>
2018-03-20 20:22:41 +0100

    Añadida configuracion inicial

---
+++ src/main/java/com/uniovi/InciDashboard_e4a/controllers/HomeController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.uniovi.InciDashboard_e4a.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String loginError(Model model) {
+		return "/index";
+	}
+
+}
