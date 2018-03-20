186d9b7f96e398f634626c4348d52c33be5ead0d
Pablo Amorin Triana <pabloarrobaconb@gmail.com>
2018-03-20 19:08:27 +0100

    Iniciado el repositorio

---
+++ src/main/java/com/uniovi/InciDashboard_e4a/InciDashboardE4aApplication.java
@@ -0,0 +1,12 @@
+package com.uniovi.InciDashboard_e4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InciDashboardE4aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InciDashboardE4aApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uniovi/InciDashboard_e4a/InciDashboardE4aApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uniovi.InciDashboard_e4a;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InciDashboardE4aApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
