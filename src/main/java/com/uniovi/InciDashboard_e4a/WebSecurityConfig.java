97fc78dfdd0e0db0a6c745c35480496fba3f36f1
Mirza Ojeda Veira <UO251443@uniovi.es>
2018-03-22 12:04:39 +0100

    Solucionados problemas de login

--- src/main/java/com/uniovi/InciDashboard_e4a/WebSecurityConfig.java
+++ src/main/java/com/uniovi/InciDashboard_e4a/WebSecurityConfig.java
@@ -6,6 +6,8 @@ package com.uniovi.InciDashboard_e4a;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,19 +28,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-				.antMatchers("/css/**", "/img/**", "/js/**", "/assets/**","/login/**").permitAll()
-				.anyRequest().authenticated()
-				.and()
-					.formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/home")
-				.and()
-					.logout().permitAll();
-		
-		//.failureUrl("/login/error")
+		http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/img/**", "/js/**", "/assets/**", "/login/**")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+				.defaultSuccessUrl("/home").and().logout().permitAll();
+
+		// .failureUrl("/login/error")
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
