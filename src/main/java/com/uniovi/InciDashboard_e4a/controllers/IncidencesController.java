03969453e27133efdf375c2e852a2a36b66b90ae
Pablo Amorin Triana <pabloarrobaconb@gmail.com>
2018-03-22 18:58:37 +0100

    Creado IncidencesController y movido a él lo que le corresponde

--- src/main/java/com/uniovi/InciDashboard_e4a/controllers/IncidencesController.java
+++ src/main/java/com/uniovi/InciDashboard_e4a/controllers/IncidencesController.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.uniovi.InciDashboard_e4a.controllers;
 
 import java.util.List;
@@ -19,33 +16,24 @@ import com.uniovi.InciDashboard_e4a.services.IncidencesService;
 import com.uniovi.InciDashboard_e4a.services.OperatorServices;
 
 @Controller
-public class HomeController {
-
+public class IncidencesController {
+	 
 	@Autowired
 	IncidencesService incidencesService;
 	@Autowired
 	OperatorServices operatorService;
-
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String index(Model model) {
-		Operator operator = operatorService.getActiveOperator();
-		List<Notification> nots = operator.getNotifications().stream().collect(Collectors.toList());
-
-		model.addAttribute("nots", nots);
-		model.addAttribute("operator", operator);
-		return "/index";
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String indexBarra(Model model) {
-		Operator operator = operatorService.getActiveOperator();
-		List<Notification> nots = operator.getNotifications().stream().collect(Collectors.toList());
-
-		model.addAttribute("nots", nots);
-		model.addAttribute("operator", operator);
-		return "/index";
-	}
-
 	
+	@RequestMapping(value = "/incidences/list", method = RequestMethod.GET)
+	public String list(Model model) {
 
+		List<Incidence> incidencias = incidencesService.getAllIncidences();
+		Operator operator = operatorService.getActiveOperator();
+		List<Notification> nots = operator.getNotifications().stream().collect(Collectors.toList());
+
+		model.addAttribute("nots", nots);
+		model.addAttribute("operator", operator);
+		model.addAttribute("allIncidences", incidencias);
+
+		return "/incidence_list";
+	}
 }
