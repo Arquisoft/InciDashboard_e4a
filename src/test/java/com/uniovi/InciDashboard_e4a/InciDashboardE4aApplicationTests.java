a7e4e012f65170c748be0f0df05c79c0c6f3da92
Mirza Ojeda Veira <UO251443@uniovi.es>
2018-03-23 10:59:47 +0100

    Mas cambios

--- src/test/java/com/uniovi/InciDashboard_e4a/InciDashboardE4aApplicationTests.java
+++ src/test/java/com/uniovi/InciDashboard_e4a/InciDashboardE4aApplicationTests.java
@@ -1,16 +1,37 @@
 package com.uniovi.InciDashboard_e4a;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InciDashboardE4aApplicationTests {
 
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testLanding() throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(containsString("Home")));
 	}
 
 }
