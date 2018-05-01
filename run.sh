dde17c97429856bc4b90b02da7b1c9b853758f9d
Antonio Paya <antonioalfa22@gmail.com>
2018-05-01 17:07:50 +0200

    Update Docker

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-cd kafka_2.11-1.1.0
-nohup bin/zookeeper-server-start.sh config/zookeeper.properties &
-echo "Zookeeper started"
-nohup bin/kafka-server-start.sh config/server.properties &
-echo "Kafka started"
-sleep 10
-cd ../project/target
+﻿cd ../project/target
 java -jar inciDashboard.jar
