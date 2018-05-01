2fd0748ab7716fa96c217ad48e755ed1c4ea50bb
Antonio Paya <antonioalfa22@gmail.com>
2018-05-01 20:26:40 +0200

    Summary

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+wget http://downloads.sourceforge.net/project/hsqldb/hsqldb/hsqldb_2_3/hsqldb-2.3.0.zip
+ls
+unzip hsqldb-2.3.0.zip
 ./hsqldb-2.3.0/hsqldb/bin/runServer.bat&
 java -jar ../project/target/InciDashboard-0.0.1.jar
