a675ca9bdedd6ce8d4e8c93646113cc48381444a
Mirza Ojeda Veira <UO251443@uniovi.es>
2018-05-02 15:15:54 +0200

    Primeros test funcionando
    Prueba para codecov

--- src/test/java/com/uniovi/InciDashboard_e4a/InciDashboardE4aApplicationTests.java
+++ src/test/java/com/uniovi/InciDashboard_e4a/InciDashboardE4aApplicationTests.java
@@ -6,10 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,6 +20,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("INTEGRATION_TEST")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class InciDashboardE4aApplicationTests {
 
 	@Autowired
@@ -30,8 +35,13 @@ public class InciDashboardE4aApplicationTests {
 	}
 
 	@Test
-	public void testLanding() throws Exception {
-		mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(containsString("Home")));
+	public void test_1_Landing() throws Exception {
+		mvc.perform(get("/")).andExpect(status().is3xxRedirection());
+	}
+
+	@Test
+	public void test_2_Login() throws Exception {
+		mvc.perform(get("/login")).andExpect(status().isOk()).andExpect(content().string(containsString("login")));
 	}
 
 }
