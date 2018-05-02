3bb0c85b135c86e2e337312525436bbbe83ed4ea
Antonio Paya <antonioalfa22@gmail.com>
2018-05-02 13:40:34 +0200

    Prueba 2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 wget http://downloads.sourceforge.net/project/hsqldb/hsqldb/hsqldb_2_3/hsqldb-2.3.0.zip
 unzip hsqldb-2.3.0.zip
 nohup bash -c "cd hsqldb-2.3.0/hsqldb/bin && java -cp ../lib/hsqldb.jar org.hsqldb.server.Server &"
-# java -jar ../project/target/InciDashboard-0.0.1.jar
+java -jar ../project/target/InciDashboard-0.0.1.jar
