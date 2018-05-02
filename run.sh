8f3d901c30bed651514c38a83acbb558ec3f5c94
Antonio Paya <antonioalfa22@gmail.com>
2018-05-02 12:59:35 +0200

    n

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 wget http://downloads.sourceforge.net/project/hsqldb/hsqldb/hsqldb_2_3/hsqldb-2.3.0.zip
 unzip hsqldb-2.3.0.zip
-add-apt-repository ppa:ubuntu-wine/ppa
-apt-get update
-apt-get install wine1.6 winetricks
 nohup bash -c "cd hsqldb-2.3.0/hsqldb/bin && wineconsole runServer.bat &"
 java -jar ../project/target/InciDashboard-0.0.1.jar
