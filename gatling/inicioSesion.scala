4acf5ba6891a93223f40c9e1400353c41466a8a9
Antonio Paya <antonioalfa22@gmail.com>
2018-05-04 20:51:22 +0200

    Add gatling test

---
+++ gatling/inicioSesion.scala
@@ -0,0 +1,28 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class RecordedSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://detectportal.firefox.com")
+		.inferHtmlResources()
+		.acceptHeader("*/*")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-ES")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0")
+
+	val headers_0 = Map("Pragma" -> "no-cache")
+
+    val uri1 = "http://detectportal.firefox.com/success.txt"
+
+	val scn = scenario("RecordedSimulation")
+		.exec(http("request_0")
+			.get("/success.txt")
+			.headers(headers_0))
+
+	setUp(scn.inject(rampUsers(200) over(120 seconds))).protocols(httpProtocol)
+}
