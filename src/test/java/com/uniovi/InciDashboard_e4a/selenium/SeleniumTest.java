6efbaa8ea830b39fcc8adfe622c49bcd37356be5
Iván Casielles Álvarez <ivanyaiza@gmail.com>
2018-05-07 20:18:13 +0200

    Test de selenium no funcionaban y ya si

--- src/test/java/com/uniovi/InciDashboard_e4a/selenium/SeleniumTest.java
+++ src/test/java/com/uniovi/InciDashboard_e4a/selenium/SeleniumTest.java
@@ -2,18 +2,27 @@ package com.uniovi.InciDashboard_e4a.selenium;
 
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.uniovi.InciDashboard_e4a.InciDashboardE4aApplication;
 import com.uniovi.InciDashboard_e4a.selenium.pageobjects.PO_LoginView;
+import com.uniovi.InciDashboard_e4a.selenium.pageobjects.PO_NavView;
 import com.uniovi.InciDashboard_e4a.selenium.pageobjects.PO_View;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = InciDashboardE4aApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SeleniumTest {
   private WebDriver driver;
   private String baseUrl;
   private StringBuffer verificationErrors = new StringBuffer();
 
+  
   @Before
   public void setUp() throws Exception {
     driver = new HtmlUnitDriver();
@@ -34,6 +43,16 @@ public class SeleniumTest {
 		PO_LoginView.fillForm(driver, "maria@prueba.es", "12345678");
 		PO_View.checkElement(driver, "label", "Username");
 	}
+	
+//	@Test
+//	public void testCambiarEstado() {
+//		PO_LoginView.fillForm(driver, "maria@prueba.es", "12345678");
+//		driver.navigate().to("http://localhost:8090/incidences/6");
+//		WebElement boton = driver.findElement(By.id("warning"));
+//		boton.click();
+//		String status = driver.findElement(By.id("status6")).getText();
+//		assertEquals( "IN_PROCESS", status);
+//	}
 
   @After
   public void tearDown() throws Exception {
